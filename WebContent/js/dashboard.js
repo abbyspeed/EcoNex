f4ad597444c4b8c6d402533e89c1938a27a60055
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2023-12-01 01:25:45 +0800

    rebase

---
+++ WebContent/js/dashboard.js
@@ -0,0 +1,6 @@
+const body = document.querySelector("body"),
+	  sideBar = body.querySelector(".sideBar");
+
+	  toggle.addEventListener("click", () => {
+		  sideBar.classList.toggle("close");
+	  });
